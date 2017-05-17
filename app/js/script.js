fdb68a88d8ff3bacf66678822f711b968b7afa76
Денис <corvus007@yandex.ru>
2017-05-17 22:53:00 +0300

    Реализует фильтрацию по событиям

--- app/js/script.js
+++ app/js/script.js
@@ -18,7 +18,31 @@ function setMaxHeight(elements) {
 }
 
 
+function getHashFilter() {
+  var hash = location.hash;
+  // get filter=filterName
+  var matches = location.hash.match(/filter=([^&]+)/i);
+  var hashFilter = matches && matches[1];
+  return hashFilter && decodeURIComponent(hashFilter);
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
+
+  var mainNavLinks = document.querySelectorAll('.main-nav__link');
+  var pageURL = location.pathname;
+  var mainNavItem = null;
+
+
+  Array.prototype.forEach.call(mainNavLinks, function(item) {
+    mainNavItem = item.parentElement;
+
+    if (~item.href.indexOf(pageURL)) {
+      mainNavItem.classList.add('main-nav__item--active');
+    } else {
+      mainNavItem.classList.remove('main-nav__item--active');
+    }
+  });
 
   /*====================================
   =            Index slider            =
@@ -77,6 +101,53 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   /*=====  End of Mmenu  ======*/
+
+
+  /*=============================================
+  =            Filter archive events            =
+  =============================================*/
+
+  var archiveEventsList = document.querySelector('.archive-events__list');
+
+  if (archiveEventsList) {
+    var $grid = $(archiveEventsList).isotope({
+      layoutMode: 'vertical'
+    });
+
+    var $filters = $('.archive-events__filter').on('click', 'a', function(event) {
+      event.preventDefault();
+      var filterAttr = $(this).attr('data-filter');
+      // set filter in hash
+      location.hash = 'filter=' + encodeURIComponent(filterAttr);
+    });
+
+    var isIsotopeInit = false;
+
+    function onHashchange() {
+      var hashFilter = getHashFilter();
+      if (!hashFilter && isIsotopeInit) {
+        return;
+      }
+      isIsotopeInit = true;
+      // filter isotope
+      $grid.isotope({
+        layoutMode: 'vertical',
+        itemSelector: '.element-item',
+        filter: hashFilter
+      });
+      // set selected class on button
+      if (hashFilter) {
+        $filters.find('.archive-events__filter-control--active').removeClass('archive-events__filter-control--active');
+        $filters.find('[data-filter="' + hashFilter + '"]').addClass('archive-events__filter-control--active');
+      }
+    }
+
+    $(window).on('hashchange', onHashchange);
+    // trigger event handler to init Isotope
+    onHashchange();
+  }
+
+  /*=====  End of Filter archive events  ======*/
 
 
 
